6f85836e032c7ebf96ece2d564bc5fb235c88657
KimJaeGeun <jgk9612@gmail.com>
2022-11-15 23:30:51 +0900

    feat: Add vim setting script

---
+++ vim/vim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# config 복사
+cd vim
+
+cp ./.vimrc ~/.vimrc
+
+# setting jellybeans
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+
+# 적용작업 필요
+# cd ~/.vimrc
+# :source %
+# :PluginInstall
