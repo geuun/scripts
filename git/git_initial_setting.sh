bb39623c2fd255d10ca869ee4c73c32749e2fe4b
KimJaeGeun <jgk9612@gmail.com>
2022-11-21 01:10:23 +0900

    refactor: Modify cofig

--- git/git_initial_setting.sh
+++ git/git_initial_setting.sh
@@ -3,11 +3,8 @@
 read -p "Github User Name: " 	UserName
 read -p "Github User Email: " 	UserEmail
 
-echo "${UserName}"
-echo "${UserEmail}"
-
-#git config --global user.name "${UserName}"
-#git config --global user.email "${UserEmail}"
-#git config --global core.editor "vim"
-#git config --global core.pager "cat"
-#git config --global init.defaultBranch main
+git config --global user.name "${UserName}"
+git config --global user.email "${UserEmail}"
+git config --global core.editor "vim"
+git config --global core.pager "cat"
+git config --global init.defaultBranch main
