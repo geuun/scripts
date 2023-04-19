8dd2e8a3abaf1e9321806ed7f5ab09becbbc007f
KimJaeGeun <jgk9612@gmail.com>
2023-04-19 22:53:29 +0900

    feat: 로그인 정보 저장

--- git/git_initial_setting.sh
+++ git/git_initial_setting.sh
@@ -8,3 +8,6 @@ git config --global user.email "${UserEmail}"
 git config --global core.editor "vim"
 git config --global core.pager "cat"
 git config --global init.defaultBranch main
+
+# git 로그인 정보 저장
+git config credential.helper store
