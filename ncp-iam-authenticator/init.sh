4ae981ad2a9cf20cc17bd775325de2a4c8dc5df8
Geuun <jgk9612@gmail.com>
2024-07-16 23:02:59 +0900

    refactor: ncp-iam script 수정

--- ncp-iam-authenticator/init.sh
+++ ncp-iam-authenticator/init.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
 
 # install ncp-iam-authenticator
-NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncp-iam-authenticator
+NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncloud
+NCP_IAM_AUTHENTICATOR_PATH=$NCP_IAM_AUTHENTICATOR_DIR/ncp-iam-authenticator
 
-## add directory 
+# add directory 
 mkdir -p $NCP_IAM_AUTHENTICATOR_DIR
 
 # check cpu architecture
 CPU_ARCH=$(uname -m)
-## install
+# install
 if [ "$CPU_ARCH" = "x86_64" ]; then
     # linux
     echo "Current Machine CPU Architecture: x86_64"
-    curl -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_linux_amd64 -o $NCP_IAM_AUTHENTICATOR_DIR/ncp-iam-authenticator
+    curl -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_linux_amd64 -o $NCP_IAM_AUTHENTICATOR_PATH
 elif [ "$CPU_ARCH" = "arm64" ]; then
     # macos
     echo "Current Machine CPU Architecture: arm64"
-    curl -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_darwin_amd64 -o $NCP_IAM_AUTHENTICATOR_DIR/ncp-iam-authenticator
+    curl -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_darwin_amd64 -o $NCP_IAM_AUTHENTICATOR_PATH
 else
     echo "Not supported architecture"
     exit 1
 fi
 
 # add binary permission
-echo "chmod +x $NCP_IAM_AUTHENTICATOR_DIR/ncp-iam-authenticator"
-chmod +x $NCP_IAM_AUTHENTICATOR_DIR/ncp-iam-authenticator
+echo "chmod +x $NCP_IAM_AUTHENTICATOR_PATH"
+chmod +x $NCP_IAM_AUTHENTICATOR_PATH
 echo "chmod success"
 
 # add PATH 
-## check current shell
+# check current shell
 CURRENT_SHELL=$(basename "$SHELL")
 
 echo "Current shell: $CURRENT_SHELL"
 
 if [ "$CURRENT_SHELL" = "bash" ]; then
-    if ! grep -q 'export NCP_IAM_DIR=$HOME/.ncp-iam-authenticator' ~/.bash_profile; then
-        echo 'export NCP_IAM_DIR=$HOME/.ncp-iam-authenticator' >> ~/.bash_profile
-        echo 'export PATH=$PATH:$NCP_IAM_DIR' >> ~/.bash_profile
+    if ! grep -q 'export NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncloud' ~/.bash_profile; then
+        echo 'export NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncloud' >> ~/.bash_profile
+        echo 'export PATH=$PATH:$NCP_IAM_AUTHENTICATOR_DIR' >> ~/.bash_profile
         echo "path added!"
     fi
 elif [ "$CURRENT_SHELL" = "zsh" ]; then 
-    if ! grep -q 'export NCP_IAM_DIR=$HOME/.ncp-iam-authenticator' ~/.zshrc; then
+    if ! grep -q 'export NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncloud' ~/.zshrc; then
         echo '' >> ~/.zshrc
-        echo 'export NCP_IAM_DIR=$HOME/.ncp-iam-authenticator' >> ~/.zshrc
-        echo 'export PATH=$PATH:$NCP_IAM_DIR' >> ~/.zshrc
+        echo 'export NCP_IAM_AUTHENTICATOR_DIR=$HOME/.ncloud' >> ~/.zshrc
+        echo 'export PATH=$PATH:$NCP_IAM_AUTHENTICATOR_DIR' >> ~/.zshrc
         echo "path added!"
     fi
 else
